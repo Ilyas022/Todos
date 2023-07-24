4360f8eadd5979b2bf3c6b44cea3684d93173ffa
Ilyas022 <ilya.shchetko@mail.ru>
2023-07-24 11:46:35 +0300

    Feature: Add saving info to local storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import todosSlice from './todoSlice'
 import tagSlice from './tagSlice'
 
-export const store = configureStore({
-  reducer: {
-    todos: todosSlice.reducer,
-    tags: tagSlice.reducer,
-  },
+const rootReducer = combineReducers({
+  todos: todosSlice.reducer,
+  tags: tagSlice.reducer,
 })
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
